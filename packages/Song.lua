ff5dea8199e78b082114ada7c1b16e846a01937b
bobtheblob <89306695+bobtheblob@users.noreply.github.com>
2022-02-03 16:41:18 +0700

    Update Song.lua

--- packages/Song.lua
+++ packages/Song.lua
@@ -1,45 +1,4 @@
-local Module = {}
-Module.Name = "Song"
-Module.ClassName = "Song"
-Module.vars = {
-	msid = 0,
-	pit = 1,
-	vol = 1,
-	snd = Instance.new("Sound")
-}
-Module.respawnfuncs = {function(self)
-	self.vars.snd = main.handle:FindFirstChild("Sound")
-end}
-Module.removingfuncs = {function(self)
-	self.vars.snd:Destroy()
-	pcall(function()
-		startermain.handle:FindFirstChild("Sound"):Destroy()
-	end)
-end}
-Module.initfuncs = {function(self)
-	self.vars.snd.Parent = main.handle
-	self.vars.snd:Clone().Parent = startermain.handle
-end}
-Module.loopfuncs = {function(self)
-	if self.vars.snd then
-		self.vars.snd.SoundId = "rbxassetid://"..self.vars.msid
-		self.vars.snd.Pitch = self.vars.pit
-		self.vars.snd.Volume = self.vars.vol
-		self.vars.snd.Playing = main.handle:IsDescendantOf(workspace)
-		self.vars.snd.Looped = true
-	end
-end}
-Module.play = function(self,args)
-	self.vars.msid = tonumber(args[1])
-	
-end
-Module.vol = function(self,args)
-	self.vars.vol = tonumber(args[1])
-end
-Module.pit = function(self,args)
-	self.vars.pit = tonumber(args[1])
-end
-return Module
+
 local Module = {}
 Module.Name = "Song"
 Module.ClassName = "Song"
