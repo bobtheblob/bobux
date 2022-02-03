c76ec8fbf221a1ae83a00c85acfa4dcd2e8fa3c5
bobtheblob <89306695+bobtheblob@users.noreply.github.com>
2022-02-03 21:05:56 +0700

    Update Song.lua

--- packages/Song.lua
+++ packages/Song.lua
@@ -5,27 +5,38 @@ getfenv().SongVars = getfenv().SongVars or {
 	msid = 0,
 	pit = 1,
 	vol = 1,
-	snd = Instance.new("Sound"),
+	snd = nil,
 	sndc = nil,
 	timepos = 0
 }
 Module.vars = getfenv().SongVars
 Module.respawnfuncs = {function(self)
-	getfenv().SongVars.snd = main.handle:FindFirstChild("Sound")
+	wait(1/60)
+	if self.vars.parent == 'player' then
+		self.vars.snd.Parent = owner.Character.PrimaryPart
+	else
+		self.vars.snd.Parent = main.handle
+	end
 end}
 Module.removingfuncs = {function(self)
-	getfenv().SongVars.snd:Destroy()
 	pcall(function()
-		startermain.handle:FindFirstChild("Sound"):Destroy()
+		getfenv().SongVars.snd:Destroy()
 	end)
+	getfenv().SongVars.snd = nil
+	pcall(function()
+		getfenv().SongVars.sndc:Destroy()
+	end)
+	getfenv().SongVars.sndc = nil
 end}
 Module.initfuncs = {function(self)
+	getfenv().SongVars.snd = getfenv().SongVars.snd or Instance.new("Sound")
 	getfenv().SongVars.snd.Parent = main.handle
 	getfenv().SongVars.snd.TimePosition = getfenv().SongVars.timepos
 	local c = getfenv().SongVars.snd:Clone()
 	getfenv().SongVars.sndc = c
 	getfenv().SongVars.sndc.TimePosition = getfenv().SongVars.timepos
 	c.Parent = startermain.handle
+	self.vars = getfenv().SongVars
 end}
 Module.loopfuncs = {function(self)
 	getfenv().SongVars = getfenv().SongVars or {
@@ -41,13 +52,29 @@ Module.loopfuncs = {function(self)
 		self.vars.snd.SoundId = "rbxassetid://"..self.vars.msid
 		self.vars.snd.Pitch = self.vars.pit
 		self.vars.snd.Volume = self.vars.vol
-		self.vars.snd.Playing = main.handle:IsDescendantOf(workspace)
+		self.vars.snd.Playing = self.vars.snd:IsDescendantOf(workspace)
+		if self.vars.parent == 'player' then
+			self.vars.snd.Parent = owner.Character.PrimaryPart
+		else
+			self.vars.snd.Parent = owner.Character.PrimaryPart
+		end
 		self.vars.snd.Looped = true
 		self.vars.sndc = self.vars.snd.TimePosition
 		self.vars.timepos = self.vars.snd.TimePosition
 	end
 end}
+Module.parent2char = function(self,args)
+	getfenv().SongVars.parent = 'player'
+	self.vars.snd.Parent = getfenv().whoplayer.Character.PrimaryPart
+end
+Module.parent2tool = function(self,args)
+	getfenv().SongVars.parent = 'tool'
+	self.vars.snd.Parent = getfenv().whoplayer.Character.PrimaryPart
+end
 Module.reinit = function(self,args)
+	for i,v in pairs(self.removingfuncs) do
+		v(self)
+	end
 	for i,v in pairs(self.initfuncs) do
 		v(self)
 	end
@@ -70,3 +97,4 @@ Module.pit = function(self,args)
 	getfenv().SongVars.pit = tonumber(args[1])
 end
 return Module
+
