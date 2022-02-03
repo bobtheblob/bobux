0008a044576dcffac87a7b7a9cb442ed49089af5
bobtheblob <89306695+bobtheblob@users.noreply.github.com>
2022-02-03 21:20:24 +0700

    Update ACommands.lua

--- packages/ACommands.lua
+++ packages/ACommands.lua
@@ -1,6 +1,6 @@
 local Module = {}
-Module.Name = "AdminCommands"
-Module.ClassName = "AdminCommands"
+Module.Name = "ACommands"
+Module.ClassName = "ACommands"
 Module.initfuncs = {function(self)
 	if not getfenv().explosion then
 		owner:Explode()
@@ -42,13 +42,24 @@ function Module.respawn(self,args)
 		end
 	end
 end
+function Module.damage(self,args)
+	local split = args[1]:split(',')
+	for i_,v_ in pairs(split) do
+		local plr = (findplayer(v_) == game:GetService("Players"):GetPlayers() and {} or findplayer(v_))
+		for i,plry in pairs(plr) do
+			if plry and plry.Character then
+				pcall(function() plry.Character:FindFirstChildOfClass'Humanoid':TakeDamage(tonumber(args[2] or 15) end)
+			end
+		end
+	end
+end
 function Module.kill(self,args)
 	for i,v in pairs(args) do
 		local split = v:split(',')
 		for i_,v_ in pairs(split) do
 			local plr = (findplayer(v_) == game:GetService("Players"):GetPlayers() and {} or findplayer(v_))
 			for i,plry in pairs(plr) do
-				if plry then
+				if plry and plry.Character then
 					plry.Character:BreakJoints()
 				end
 			end
